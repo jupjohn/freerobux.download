3ccf96a24d72f318d3ce86cd4a3f25d48d483323
James Upjohn <jupjohn@jammeh.co.nz>
2024-08-30 14:26:54 +1200

    feat: add middleware to fetch images from bucket

---
+++ functions/_middleware.js
@@ -0,0 +1,14 @@
+export async function onRequestGet(context) {
+  const possibleImageName = new URL(context.request.url).pathname.replace(/^\//, "");
+  if (possibleImageName === "") {
+    return await context.next();
+  }
+
+  const obj = await context.env.IMAGE_STORE.get(possibleImageName);
+
+  if (obj === null) {
+    return await context.next();
+  }
+
+  return new Response(obj.body);
+}
